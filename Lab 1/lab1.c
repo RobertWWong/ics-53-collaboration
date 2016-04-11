4752e035e7a76de1e3f2bc2d7ccd0eb7cdac4414
archon263 <archon263@gmail.com>
2016-04-11 12:48:52 -0700

    Fantastic results in completing read. Now need to do write. Use a struct array for global. Need to work on gather input from users with switch statements. Yay.

--- Lab 1/lab1.c
+++ Lab 1/lab1.c
@@ -1,4 +1,13 @@
 #include "stdio.h"
+#include <string.h>
+
+//taking a long time to compile and i don't know why. Is it because of the for loop?
+	struct string {
+		char str[1024];
+	};
+	struct string global_c[100];
+	int cnt;
+	
 
 //Your record keeper program should prompt the user for a command, perform the command, 
 //and then prompt for the next command. Your program should support the following 5 commands:
@@ -6,31 +15,48 @@
 void read (char read_file[]){	//arg is a string
 	// Read a record file from disk and represent its contents in memory using data structures of your choosing.
 	// This command takes one argument, the name of a record file to be read. This command does not print any output. 
+	extern struct string global_c[];
+	extern int cnt;
+	
+	struct string temp;
 
-	printf ("Here you go %s\n", read_file);
+	printf ("Reading from %s", read_file);
+
 	FILE *fp;
 	char buff [255];
-
 	fp = fopen(read_file, "r");
-
-	fgets (buff, 255, (FILE*)fp); //don't understand the (FILE*) part
-
-	printf("Current we have this %s\n  ", buff);		//think of printf as your format, they will appear at your % signs specifically.
-
-	fclose(fp);
+	while (fgets (buff, 255, (FILE*)fp))
+	{
+		strcpy (temp.str, buff);
+		global_c [cnt++] = temp;
+		printf("Current we have this %s\n", buff);		//think of printf as your format, they will appear at your % signs specifically.
+		//fgets (buff, 255, (FILE*)fp); //don't understand the (FILE*) part
+	}
 	
+	fclose(fp);
 }
 
-void write (char *write_file[]){
+void write (char write_file[]){
  // Write a record file to disk which contains the records currently stored in memory using data structures of your choice. 
  // This command takes one argument, the name of the record file to be written to disk. The command does not print any output. 
+	extern struct string global_c[];
+	extern int cnt;
+	FILE *fp;
+	fp = fopen(write_file, "a");
+	fclose(fp);
 }
 
 void print () {
 	 // Print all of the records currently loaded into memory, in order. 
 	 // Each record should be printed on a single line and should be prefixed with a unique number 
 	 // which indicates its order with respect to the other records. 
+	
 
+/*
+	for (int i = 0; i <cnt; i++){
+		printf("%d %s", i+1, global_c[i].str );
+	}
+*/
 }
 
 void delete () {
@@ -43,19 +69,23 @@ void quit() {
 
 }
 
-
 int main(){
 
-  printf("Starting Program addrs: \n");
-	
-	char str[100];
-	
-	printf ("Now enter your command: ");
-	scanf ("%s" , str);		//will only read as many string as you input it
-	printf("\nHere's the command, it is : %s", str);	//will return just one string
-	printf("\n");
+	printf("Starting Program addrs: \n");
+	struct string global_c[100];
+	int cnt = 0;
 
+	char str[100];
+	while (strcmp("quit", str))
+	{
+	printf ("Your command: ");
+	scanf ("%s" , str);		//will only read as many string as you input it
+	printf("%s", str);	//will return just one string
+
+
+}
 	read(str);
+	print();
   
   
   return 0;
---
+++ Lab 1/var.h
@@ -0,0 +1,10 @@
+#ifndef var
+#define var
+
+	struct string {
+		char str[1024];
+	};
+	struct string global_c[255];
+	int cnt = 0;
+
+#endif
